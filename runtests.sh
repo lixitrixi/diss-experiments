95601dca26f2d6602ad476f385fb54b305b5d1c7
Felix <felix@felixleitner.com>
2025-12-04 13:32:59 +0000

    add rewriter time to conjure oxide output

--- runtests.sh
+++ runtests.sh
@@ -29,8 +29,13 @@ bench_oxide() {
 
     info "DONE: $probfile"
 
+    secs=$(jq '.stats.rewriterRuns.[0].rewriterRunTime.secs' < $statfile)
+    nanos=$(jq '.stats.rewriterRuns.[0].rewriterRunTime.nanos' < $statfile)
+    rewritetime=$(echo "$secs + $nanos/10^9" | bc -l)
+
     solvetime=$(jq '.stats.solverRuns.[0].conjureSolverWallTime_s' < $statfile)
-    echo "conjure-oxide, $solver, $probfile, $3, $solvetime"
+
+    echo "conjure-oxide, $solver, $probfile, $3, $solvetime, $rewritetime"
 }
 
 # Run Conjure and print the solver time
@@ -44,7 +49,7 @@ export ERR_FILE
 export -f bench_conjure bench_oxide info err
 
 mkdir -p output
-echo "tool, solver, problem, repeat_iteration, solver_wall_time_s" > $RESULTS_FILE
+echo "tool, solver, problem, repeat_iteration, solver_wall_time_s, rewriter_wall_time_s" > $RESULTS_FILE
 
 parallel --progress '{}' \
     :::: cmd-prefixes \
