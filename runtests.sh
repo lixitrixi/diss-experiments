7689aabc6567330c284e39730ef06fea80bdbfb8
Felix <felix@felixleitner.com>
2025-12-04 16:12:34 +0000

    measure rewrite time and benchmark conjure

--- runtests.sh
+++ runtests.sh
@@ -6,6 +6,7 @@ REPEATS=1
 NOW="$(date +%Y-%m-%d_%H-%M-%S)"
 ERR_FILE="output/err_$NOW.log"
 RESULTS_FILE="output/results_$NOW.csv"
+COLUMNS="tool, solver, problem, repeat_iteration, solver_wall_time_s, rewriter_wall_time_s"
 
 info() {
   echo "-- info: $*" >/dev/stderr
@@ -27,13 +28,13 @@ bench_oxide() {
         --info-json-path $statfile \
         $probfile > /dev/null 2>> $ERR_FILE
 
-    info "DONE: $probfile"
+    info "DONE (oxide): $probfile"
 
-    secs=$(jq '.stats.rewriterRuns.[0].rewriterRunTime.secs' < $statfile)
-    nanos=$(jq '.stats.rewriterRuns.[0].rewriterRunTime.nanos' < $statfile)
+    secs=$(jq -r '.stats.rewriterRuns.[0].rewriterRunTime.secs' < $statfile)
+    nanos=$(jq -r '.stats.rewriterRuns.[0].rewriterRunTime.nanos' < $statfile)
     rewritetime=$(echo "$secs + $nanos/10^9" | bc -l)
 
-    solvetime=$(jq '.stats.solverRuns.[0].conjureSolverWallTime_s' < $statfile)
+    solvetime=$(jq -r '.stats.solverRuns.[0].conjureSolverWallTime_s' < $statfile)
 
     echo "conjure-oxide, $solver, $probfile, $3, $solvetime, $rewritetime"
 }
@@ -41,6 +42,22 @@ bench_oxide() {
 # Run Conjure and print the solver time
 bench_conjure() {
     probfile=$1
+    outdir=$(mktemp -d)
+
+    conjure solve \
+        --solver z3 \
+        -o $outdir \
+        --copy-solutions=off \
+        $probfile > /dev/null 2>> $ERR_FILE
+    
+    info "DONE (conjure): $probfile"
+
+    solvetime=$(jq -r '.savilerowInfo.SolverTotalTime' < $outdir/*.stats.json)
+
+    totaltime=$(jq -r '.totalTime' < $outdir/*.stats.json)
+    rewritetime=$(echo "$totaltime - $solvetime" | bc -l)
+
+    echo "conjure, z3, $probfile, $2, $solvetime, $rewritetime"
 }
 
 # /// Run Tests ///
@@ -49,7 +66,7 @@ export ERR_FILE
 export -f bench_conjure bench_oxide info err
 
 mkdir -p output
-echo "tool, solver, problem, repeat_iteration, solver_wall_time_s, rewriter_wall_time_s" > $RESULTS_FILE
+echo $COLUMNS > $RESULTS_FILE
 
 parallel --progress '{}' \
     :::: cmd-prefixes \
