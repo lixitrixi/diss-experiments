7a09b9b5a9006aedc46ab9ad3f281f4168b5a6f6
Felix <felix@felixleitner.com>
2025-12-23 12:43:10 -0500

    add solver profile to unwrap alldiffs

--- cmd-prefixes.sh
+++ cmd-prefixes.sh
@@ -2,6 +2,10 @@ bench_oxide smt-lia-arrays
 bench_oxide smt-lia-atomic
 bench_oxide smt-bv-arrays
 bench_oxide smt-bv-atomic
+bench_oxide smt-lia-arrays-nodiscrete
+bench_oxide smt-lia-atomic-nodiscrete
+bench_oxide smt-bv-arrays-nodiscrete
+bench_oxide smt-bv-atomic-nodiscrete
 bench_conjure z3-lia 2
 bench_conjure z3-bv 2
 bench_conjure z3-lia 0
